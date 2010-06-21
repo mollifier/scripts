0e85e8954a00ea9cf37cfd6838e07ebbf64dbac7
mollifier <mollifier@gmail.com>
2010-06-22 01:27:17 +0900

    Change to use -n.

--- prism-gmail.sh
+++ prism-gmail.sh
@@ -6,7 +6,7 @@ declare -r FIREFOX_PATH="/usr/bin/firefox"
 declare -r FIREFOX_PROFILE_DIR_PATH="${HOME}/.mozilla/firefox"
 declare -r FIREFOX_DEFAULT_PROFILE_DIR_PATH="$(find $FIREFOX_PROFILE_DIR_PATH -maxdepth 1 -type d -name '*.default' | head -n 1)"
 
-if [ "${FIREFOX_DEFAULT_PROFILE_DIR_PATH}" != "" ]; then
+if [ -n "${FIREFOX_DEFAULT_PROFILE_DIR_PATH}" ]; then
   "$FIREFOX_PATH" \
     -app "${FIREFOX_DEFAULT_PROFILE_DIR_PATH}/extensions/refractor@developer.mozilla.org/prism/application.ini" \
     -override "${HOME}/.webapps/gmail@prism.app/override.ini" \
